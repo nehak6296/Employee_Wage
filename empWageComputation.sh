0b16ed7e9580ce91bb4b7dc617dd6a57037e5336
nehak6296 <nehak6296@gmail.com>
2021-01-26 18:58:52 +0530

    Added DailyWage & TotalSalary

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -13,6 +13,7 @@ numWorkingDays=20;
 totalEmpHr=0;
 totalWorkingDays=0;
 workingHours=0;
+empDailyWage=0;
 
 function getWorkingHours() {
 	local empCheck=$1
@@ -32,13 +33,21 @@ function getWorkingHours() {
 
 }
 
-while [[ $totalEmpHr -lt $max_hrs_in_month &&
-        $totalWorkingDays -lt $numWorkingDays ]]
+function getWage() {
+	local workingHour=$1
+	echo $(($workingHours*$empRatePerHr))
+}
+
+while [[ $totalEmpHr -lt $max_hrs_in_month && $totalWorkingDays -lt $numWorkingDays ]]
 do
         ((totalWorkingDays++))
-        workingHours=$( getWorkingHours $((RANDOM%3)) );
-        (( totalEmpHr+=workingHours ))
-
+        workingHours="$( getWorkingHours $((RANDOM%3)) )";
+        totalEmpHr=$(($totalEmpHr + $workingHours))
+	empDailyWage[$totalWorkingDays]="$( getWage $workingHour )"
+	
 done
-salary=$(( totalEmpHr*empRatePerHr ))
-echo "salary per month is $salary"
+	echo ${empDailyWage[@]}
+	totalSalary=$(( totalEmpHr*empRatePerHr ))
+	empDailyWage[$totalWorkingDays]="$totalSalary"
+	echo "Added totalSalary " 
+	echo ${empDailyWage[@]}
