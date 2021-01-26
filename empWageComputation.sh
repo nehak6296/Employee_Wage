54a375c77cfbb4aa3060ed76d147f86cc20e2814
nehak6296 <nehak6296@gmail.com>
2021-01-26 12:45:08 +0530

    Add Employee Wage

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
-is_Present=1;
-randomCheck=$((RANDOM % 2))
+isPresent=1;
 
-	if [ $is_Present = 1 ];
-        then
-                echo "Employee is present.."
-        else
-                echo "Employee is abscent.."
-        fi
+randomCheck=$((RANDOM%2))
+
+if [ $isPresent -eq $randomCheck ]
+then
+	empRatePerHr=20
+        empHrs=8;
+	salary=$(($empHrs*$empRatePerHr))
+else
+        salary=0;
+fi
+
+echo $salary
